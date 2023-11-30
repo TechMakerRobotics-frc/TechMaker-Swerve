f88dd9ddb23b40dde29ca1d32bb295a8ad7e69f7
TechMaker <sesi.techmakerrobotics@gmail.com>
2023-11-30 14:16:32 -0300

    TechMaker Swerve

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,111 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.commands.swerve.*;
+
+import java.io.File;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer
+{
+    // The robot's subsystems and commands are defined here...
+    private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+
+    private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+    
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    CommandXboxController driverXbox = SingletonCommandXboxController.INSTANCE;
+
+
+    AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(drivebase,
+            // Applies deadbands and inverts controls because joysticks
+            // are back-right positive while robot
+            // controls are front-left positive
+            () -> MathUtil.applyDeadband(driverXbox.getLeftY(),
+                    OperatorConstants.LEFT_Y_DEADBAND),
+            () -> MathUtil.applyDeadband(driverXbox.getLeftX(),
+                    OperatorConstants.LEFT_X_DEADBAND),
+            () -> -driverXbox.getRightX(),
+            () -> -driverXbox.getRightY(),
+            false);
+
+    TeleopDrive closedTeleopDrive = new TeleopDrive(drivebase,
+            () -> driverXbox.getLeftX(),
+            () -> -driverXbox.getLeftY(),
+            () -> -driverXbox.getRightX(),
+            () -> driverXbox.getHID().getLeftBumper(),
+            false,
+            false
+    );
+
+
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer()
+    {
+        // Configure the trigger bindings
+        configureBindings();
+
+
+
+        drivebase.setDefaultCommand(closedTeleopDrive);
+    }
+    
+    
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings()
+    {
+        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+        new Trigger(exampleSubsystem::exampleCondition)
+                .onTrue(new ExampleCommand(exampleSubsystem));
+
+        driverXbox.rightBumper().onTrue(new InstantCommand(drivebase::zeroGyro));
+
+        driverXbox.a().toggleOnTrue(closedAbsoluteDrive);
+        
+        // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+    }
+    
+    
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand()
+    {
+        // An example command will be run in autonomous
+        return Autos.exampleAuto(exampleSubsystem);
+    }
+}
