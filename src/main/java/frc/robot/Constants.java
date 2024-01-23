6c1a5c9dfa6e459d77d9d1efbe54fe1d42cda66a
TechMaker <sesi.techmakerrobotics@gmail.com>
2024-01-23 14:41:22 -0300

    PID Heading, implementado com sucesso sem teste.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,22 +1,9 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
-//import swervelib.parser.PIDFConfig;
-
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
- * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
+ 
 public final class Constants
 {
 
@@ -32,6 +19,13 @@ public final class Constants
         
         // P = 0.46 - I = 0.17 - D = 0.01 
 
+        public static final double kpH = 0;
+        public static final double kiH = 0;
+        public static final double kdH = 0;
+
+        // P = 0 - I = 0 - D = 0 
+
+
        /* velForward = drivePID.calculate(limelight.getALimelight(), strafeOffSet);
         velStrafe = strafePID.calculate(limelight.getXLimelight(), strafeOffSet);
         velGiro = rotationPID.calculate(limelight.getYaw(),rotationOffset); */
@@ -43,9 +37,7 @@ public final class Constants
 
     public static final class Drivebase
     {
-
-        // Hold time on motor brakes when disabled
-        public static final double WHEEL_LOCK_TIME = 10; // seconds
+public static final double WHEEL_LOCK_TIME = 10; // seconds
     }
 
     public static class OperatorConstants
